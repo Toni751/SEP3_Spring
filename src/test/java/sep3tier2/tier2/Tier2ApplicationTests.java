3510ee3417bdf721cf217cf0fcc01dd47a1c3555
Toni <antonio.grama10@yahoo.com>
2020-11-07 15:40:11 +0100

    Initial commit

---
+++ src/test/java/sep3tier2/tier2/Tier2ApplicationTests.java
@@ -0,0 +1,13 @@
+package sep3tier2.tier2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tier2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
