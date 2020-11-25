51c388f48cedc9ead9135c2bcdab43680a57c6a2
Toni <antonio.grama10@yahoo.com>
2020-11-25 17:39:04 +0100

    Small notifications fix

--- src/main/java/sep3tier2/tier2/controllers/websockets/NotificationsController.java
+++ src/main/java/sep3tier2/tier2/controllers/websockets/NotificationsController.java
@@ -3,7 +3,9 @@ package sep3tier2.tier2.controllers.websockets;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
 import sep3tier2.tier2.models.UserAction;
 import sep3tier2.tier2.services.user.UserService;
@@ -17,17 +19,20 @@ public class NotificationsController
     @Autowired
     private SimpMessagingTemplate messagingTemplate;
 
-    @MessageMapping("/notifications")
+    @MessageMapping("/note")
+   // @SendToUser("/topic/notifications")
     public void sendNotification(@Payload UserAction userAction)
     {
         System.out.println(userAction.toString());
-        messagingTemplate.convertAndSendToUser(Integer.toString(userAction.getReceiverId()),"/queue/notifications", userAction);
-        //messagingTemplate.convertAndSend("/queue/notifications/" + userAction.getReceiverId(), userAction);
+        //messagingTemplate.convertAndSendToUser(String.valueOf(userAction.getSenderId()),"/topic/notifications", userAction);
+        messagingTemplate.convertAndSend("/topic/notifications/" + userAction.getReceiverId(), userAction);
         try {
             userService.postUserAction(userAction);
         } catch (Exception e) {
-           messagingTemplate.convertAndSend("/queue/errors/" + userAction.getSenderId(), "Could not post user action " + userAction.getActionType());
-           messagingTemplate.convertAndSend("/queue/errors/" + userAction.getReceiverId(), "Could not post user action " + userAction.getActionType());
+            System.out.println("Sending error message to clients");
+           messagingTemplate.convertAndSend("/topic/errors/" + userAction.getSenderId(), userAction.getActionType() + "_ERROR");
+           messagingTemplate.convertAndSend("/topic/errors/" + userAction.getReceiverId(), userAction.getActionType() + "_ERROR");
         }
+      // return userAction;
     }
 }
